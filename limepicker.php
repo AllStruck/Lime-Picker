2ea8fabdbfcaa718a2bab5fbb36b286741d78462
David Monaghan <monaghan.david@gmail.com>
2012-06-24 17:14:08 -0700

    Some basics

---
+++ limepicker.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Lime Picker
+Plugin URI: http://limepicker.allstruck.com
+Description: Used to create a webapp for browsing and previewing LimeChat themes.
+Version: 1.0
+Author: AllStruck
+Author URI: http://allstruck.com
+License: GPL2
+*/
+
